0e4959154cfe677d50f000b92577009b5292b066
SuperficialFlow <154480145+SuperficialFlow@users.noreply.github.com>
2024-01-14 16:40:22 +0800

    final

--- 4. Functions and Exports/index.mjs
+++ 4. Functions and Exports/index.mjs
@@ -39,18 +39,19 @@ function example(value, callBack) {
     value = 11
     callBack(value)
 }
-function add(value) {
+function addAge(value) {
     var svalue = 10
     var age = svalue + value
     console.log("I am age,", age)
 }
-example(0, add)
+example(0, addAge)
 
 // Todo 4.3 Declare a higher-order function named 'modifyArray' that takes an array and a function as parameters. The function should modify each element in the array using the provided function and return the modified array.
 // Then, Call the 'modifyArray' function with the 'numbers' array and a function that increments each number by 1
 // Useful Tutorial: https://youtu.be/H4awPsyugS0?si=7wC1B7whXVkG8X5l
 // Your code here
-numbers = [2, 4, 6, 8, 10]
+console.log("\n4.3")
+let numbers = [2, 4, 6, 8, 10]
 function modifyArray(array, afunction) {
     return array.map(afunction)
 }
@@ -64,8 +65,9 @@ console.log("With increment:", modifyArray(numbers, incrementByOne))
 // Then, import and call the add method here and display the value of the PI constant
 // Useful Tutorial: https://youtu.be/qgRUr-YUk1Q?si=ceAaeHJfCYCmeNWx
 // Your code here
-import { add, PI } from './mathUtils.js'
-const result = add(5, 7)
+console.log("\n4.4")
+import { add, PI } from './mathUtils.mjs'
+const result = add(5, 10)
 console.log("Add method: ", result)
 console.log("PI constant", PI)
 
@@ -73,7 +75,12 @@ console.log("PI constant", PI)
 // Then, import and call the isPalindrome method here
 // Useful Tutorial: https://youtu.be/qgRUr-YUk1Q?si=ceAaeHJfCYCmeNWx
 // Your code here
+console.log("\n4.5")
+import isPalindrome from './isPalindrome.mjs'
+console.log("Given word is a", isPalindrome('tenet'), "palindrome word.")
 
 // Checkpoint 4.3 What does the require function and module.exports variable do?
 // Should we use it over the import and export keywords? Why? Research your answer.
 // Answer: 
+console.log("\n4.3")
+console.log("Module.exports is a variable that reflects the current module and returns an object that can be accessed using the need method. The require function would then be an import type module that takes a module name as a parameter and includes external modules or the module's contents. Module.exports normally exports objects, which are subsequently retrieved by the require function and saved in a variable that the user can reuse. In my perspective, the require function provides more flexibility than the import function. With require, you can write the necessary statement and use it within the program. In contrast, the import function is called at the beginning of the file. Notably, import can be used asynchronously for better efficiency, while require is synchronous. Import enables the selective loading of selected components, potentially conserving memory.")
